d92564cdc374622dc040856266cd5a1149b29511
anandu86130 <anandu86130@gmail.com>
2024-10-13 12:47:08 +0530

    commit

--- internal/api/service/service.go
+++ internal/api/service/service.go
@@ -1,6 +1,9 @@
 package service
 
 import (
+	"context"
+	"fmt"
+
 	pb "github.com/anandu86130/Hospital-user-service/internal/pb"
 	usecaseint "github.com/anandu86130/Hospital-user-service/internal/usecase/interface"
 )
@@ -14,4 +17,28 @@ func NewUserServer(usecase usecaseint.UserUseCase) pb.UserServer {
 	return &UserServer{
 		UserUseCase: usecase,
 	}
+}
+
+func (u *UserServer) Signup(ctx context.Context, req *pb.SignupRequest) (*pb.SignupResponse, error) {
+	result, err := u.UserUseCase.Signup(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to signup: %v", err)
+	}
+	return result, nil
+}
+
+func (u *UserServer) VerifyOTP(ctx context.Context, req *pb.VerifyOTPRequest) (*pb.VerifyOTPResponse, error) {
+	result, err := u.UserUseCase.VerifyOTP(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to verify OTP: %v", err)
+	}
+	return result, nil
+}
+
+func (u *UserServer) Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginResponse, error) {
+	result, err := u.UserUseCase.Login(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to login: %v", err)
+	}
+	return result, nil
 }
